6a4065258ff654a7c1122cb78dc39a1ff036294f
anuzuga123-alt <anuzuga123@gmail.com>
2025-09-07 10:39:31 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "packingfriend-v3";
+const CACHE_NAME = "packingfriend-v4";
 const OFFLINE_URLS = [
   "./index.html",
   "./manifest.json",
@@ -8,17 +8,13 @@ const OFFLINE_URLS = [
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_URLS))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_URLS)));
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
   );
   self.clients.claim();
 });
@@ -27,13 +23,9 @@ self.addEventListener("fetch", event => {
   event.respondWith(
     fetch(event.request)
       .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, response.clone()));
         return response;
       })
-      .catch(async () => {
-        const cached = await caches.match(event.request);
-        return cached || caches.match("./offline.html");
-      })
+      .catch(() => caches.match(event.request).then(res => res || caches.match("./offline.html")))
   );
 });
